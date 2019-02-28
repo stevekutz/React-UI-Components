98f242ae5b97a7ae5bb442503e7e997fb0db7515
stevekutz <stkutz@gmail.com>
2019-02-27 21:22:53 -0600

    used better Lambda logo image and fixed with border-radius

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const FooterContainer = () => {
+  return (
+    <div className = 'footer-container'>
+      <div className = footerIMG>
+
+      </div>
+
+
+    </div>
+  )
+};
+
+export default FooterContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -6,8 +6,9 @@ const ImageThumbNail = () => {
     <div className = "headerIMG">
 
    <img
-     src = 'https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png'
+    /* src = 'https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png' */
     /* src = "./lambda.png"  */
+    src = 'https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg'
      alt = "lambda-logo"
    />
 
