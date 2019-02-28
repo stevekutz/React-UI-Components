9d3b0e60f7573578f85eaed6a31d485031135159
stevekutz <stkutz@gmail.com>
2019-02-27 22:53:52 -0600

    added count to recycle button

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './Footer.css';
 
 const FooterContainer = () => {
+  let count = 0;
+
+  function handleClick(e) {
+    let recycled = root.querySelector('.counter');
+    count++;
+    recycled.textContent = count.toString();
+  }
+
+
   return (
     <div className = 'footer-container'>
       <div className = 'footerIMG bubble'>
@@ -11,18 +20,33 @@ const FooterContainer = () => {
         />
       </div>
 
-      <div className = 'footerIMG recycle'>
+      <div
+        className = 'footerIMG recycle'
+      >
         <img
+          onClick={handleClick}
           src="https://img.icons8.com/ios/50/000000/synchronize.png"
           alt = 'recycle'
         />
+        <div
+          className = "counter">0</div>
       </div>
 
       <div className = 'footerIMG like'>
         <img
+          className = 'active'
           src="https://img.icons8.com/ios/50/000000/hearts.png"
-          alt = 'like'
+          alt = 'like-empty'
         />
+
+        <img
+          className = 'hide'
+          src="https://img.icons8.com/ios/50/000000/hearts-filled.png"
+          alt = 'like-full'
+        />
+
+
+
       </div>
 
       <div className = 'footerIMG send'>
