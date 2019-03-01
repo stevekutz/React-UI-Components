a3ba3da95a19fe164ee731c5149f80e696c8d21b
stevekutz <stkutz@gmail.com>
2019-02-28 19:58:58 -0600

    working on making like footer icon change on mouseover to solid and still count

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
@@ -4,12 +4,24 @@ import './Footer.css';
 const FooterContainer = () => {
   let count = 0;
 
-  function handleClick(e) {
+  function handleClick() {
     let recycled = root.querySelector('.counter');
     count++;
     recycled.textContent = count.toString();
   }
 
+  function handleIMG () {
+    let hollowHeartIMG = root.querySelector('.hollow');
+    let solidHeartIMG = root.querySelector('.solid');
+
+    hollowHeartIMG.classList.toggle('hidden');
+    solidHeartIMG.classList.toggle('active');
+
+
+  }
+
+
+  // "https://img.icons8.com/ios/50/000000/hearts-filled.png"
 
   return (
     <div className = 'footer-container'>
@@ -31,17 +43,22 @@ const FooterContainer = () => {
 
       <div
         onClick={handleClick}
+        onMouseOver={handleIMG}
+
+
         className = 'footerIMG like'>
         <img
-          className = 'active'
+          className = 'hollow active'
           src="https://img.icons8.com/ios/50/000000/hearts.png"
           alt = 'like-empty'
         />
-
+        <img
+          className = 'solid hidden'
+          src="https://img.icons8.com/ios/50/000000/hearts-filled.png"
+          alt = 'like-empty'
+        />
 
         <div className = "counter">0</div>
-
-
 
       </div>
 
