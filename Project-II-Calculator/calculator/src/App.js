bdd47015f1213a35ad6d9f1ea70b4f1c2be6bc3e
stevekutz <stkutz@gmail.com>
2019-03-01 08:05:40 -0600

    initial-  calculator setup using classes and functinal components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputs: [],
+      display: ''
+
+    };
+  }
+  render() {
+    return (
+      <div className = 'main-container'>
+          <h1>
+            LETS BUILD THIS THING
+
+
+          </h1>
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
+    );
+  }
+}
 
 export default App;
