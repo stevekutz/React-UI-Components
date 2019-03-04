f5ed1344e2b1d912f78fd922464f890f02481cee
stevekutz <stkutz@gmail.com>
2019-03-03 19:21:20 -0600

    completed major refactor of state management, finalizing math func callbacks & case statement in doMath method

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,24 +10,87 @@ class App extends React.Component {
       inputs: '',   // change this to string
       display: '',
       total: '',
+      num1: '',
+      num2: '',
       operatorLimit: false,
       firstCharNeg: false,
       operator: '',
-      cleared: 0
+      cleared: 0,
+      mathObj: {
+        val1: '',
+        val2: '',
+        operatorVal: ''
+      }
+      };
     };
-  }
+
 
   // add event handlers here
   changeHandler = event => {
-//    console.log("event name", event.target.name);
-    event.persist();  // added to see if onClick ever fired
-    let displayArr = this.state.inputs.slice();
-    console.log("event value", event.target.value);
+  //    console.log("event name", event.target.name);
+      event.persist();  // added to see if onClick ever fired
+    //  let displayArr = this.state.inputs.slice();
+      console.log("event value", event.target.value);
 
-    const val = event.target.value;
+      const val = event.target.value;
+      const math = this.state.mathObj;
 
-    console.log('!!!!!!', val !== "=" );
+      console.log('this is a number',  !isNaN(val) );
+      console.log('checkOperator says ', this.checkOperator(val) );
 
+      console.log('operatorLimit is ', this.state.operatorLimit);
+
+
+
+
+      let getNumber = (obj, i) => {
+        if( ((obj['val${i}']=== '') && (val === "−") ) ||  (!isNaN(val) ) ) {
+
+          obj[`val${i}`] += val;
+          console.log('mathObj is ', obj);
+          this.setState({inputs: obj[`val${i}`]});
+        }
+      };
+
+
+
+
+      if(!this.state.operatorLimit) {
+          getNumber(math, 1)
+      } else if ( this.checkOperator(val) ) {
+
+          console.log('OPERATOR ENTERED');
+          math[`operatorVal`] = val;     // find operator
+          this.setState({ operatorLimit: true});
+
+      } else if(this.state.operatorLimit) {
+          getNumber(math, 2)
+      } else if (val === "=") {
+          console.log(` equal sign hit before `);   // exception handle if user hit equal in wrong time
+      }
+
+
+
+  };
+
+
+    clearDisplay = () => {
+      this.setState({
+        inputs: [],
+        display: '',
+        operatorLimit: false,
+        operator: '',
+        operatorIndex: 0
+      })
+    };
+
+
+    checkOperator = (value) => {
+      return (value === "÷" || value === "×" || value === "−" || value === "+");
+    };
+
+
+/*  ***********************************************************
     // REFACTOR THIS !!!!!!
    const validValue = (val) => {
       return ( (this.checkOperator(val)
@@ -53,7 +116,7 @@ class App extends React.Component {
     // allow first char to be - & then a subtract symbol and then - - or - op
     // first char cannot be op except -
     // char after num1 is allowed to be negative
-
+/*
     let calcStr = this.state.inputs.slice();
 
 
@@ -76,26 +139,15 @@ class App extends React.Component {
         ({ operatorLimit: true,
            operator: displayArr[displayArr.length -1]
         });
-    
+*/
 
-  };
 
-  clearDisplay = () => {
-    this.setState({
-      inputs: [],
-      display: '',
-      operatorLimit: false,
-      operator: '',
-      operatorIndex: 0
-    })
-  };
 
-  checkOperator = (value) => {
-    return (value === "÷" || value === "×" || value === "−" || value === "+");
-  };
+//  };
 
+
+/*
   doMath = (str, op) => {
-
 
     // read into an array without commas, then split out on operator
 
@@ -109,6 +161,7 @@ class App extends React.Component {
 
     let num1 = 0;
     let num2 = 0;
+*/
 
 /*
     if(mathArr[0].length > 0) {
@@ -122,9 +175,9 @@ class App extends React.Component {
     console.log(num1);
     console.log(num2);
 */
-    this.clearDisplay();
+ //   this.clearDisplay();
 
-  };
+//  };
 
 
   render() {
